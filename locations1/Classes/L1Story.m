aef18b1a954c9bba6d40bc5478e4cd36a1df4a17
Joe Zuntz <joezuntz@googlemail.com>
2011-03-02 16:48:04 +0000

    Many changes from last few days to how nodes work.

---
+++ locations1/Classes/L1Story.m
@@ -0,0 +1,14 @@
+//
+//  L1Plotline.m
+//  locations1
+//
+//  Created by Joe Zuntz on 19/02/2011.
+//  Copyright 2011 Imperial College London. All rights reserved.
+//
+
+#import "L1Story.h"
+
+
+@implementation L1Story
+
+@end
