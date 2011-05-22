e89854d19d867f0b1b3389d9e1f7b43a0d2a5bc2
Joe Zuntz <joezuntz@googlemail.com>
2011-05-22 20:09:52 +0100

    added fake simulated location and perhaps fixed mem error

---
+++ locations1/Classes/SimulatedUserLocation.m
@@ -0,0 +1,14 @@
+//
+//  SimulatedUserLocation.m
+//  locations1
+//
+//  Created by Joe Zuntz on 22/05/2011.
+//  Copyright 2011 Imperial College London. All rights reserved.
+//
+
+#import "SimulatedUserLocation.h"
+
+
+@implementation SimulatedUserLocation
+
+@end
